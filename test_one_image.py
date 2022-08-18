4c754d714099ccda9e312f85feca6736fc898f38
Pappol <riccardo.parola@studenti.unitn.it>
2022-08-18 17:59:31 +0200

    benchmark

--- test_one_image.py
+++ test_one_image.py
@@ -8,6 +8,64 @@ import torch.nn.functional as F
 from torchvision import transforms
 from models.models import create_model
 from options.test_options import TestOptions
+import time
+import os
+import glob
+
+def benchmark(model, path):
+    #select list of images from folder
+    list_of_images = glob.glob(path, recursive=False)
+    print(list_of_images)
+    model.eval()
+    for i in range(list_of_images.__len__()-1):
+        start_time = time.time()
+
+        pic_a = list_of_images[i]
+        pic_b = list_of_images[i+1]
+        img_a = Image.open(pic_a).convert('RGB')
+        img_a = transformer_Arcface(img_a)
+        img_id = img_a.view(-1, img_a.shape[0], img_a.shape[1], img_a.shape[2])
+
+        img_b = Image.open(pic_b).convert('RGB')
+        img_b = transformer(img_b)
+        img_att = img_b.view(-1, img_b.shape[0], img_b.shape[1], img_b.shape[2])
+        # convert numpy to tensor
+        img_id = img_id.cuda()
+        img_att = img_att.cuda()
+        #create latent id
+        img_id_downsample = F.interpolate(img_id, size=(112,112))
+        latend_id = model.netArc(img_id_downsample)
+        latend_id = latend_id.detach().to('cpu')
+        latend_id = latend_id/np.linalg.norm(latend_id,axis=1,keepdims=True)
+
+        latend_id = latend_id.to('cuda')
+        #forward
+        img_fake = model(img_id, img_att, latend_id, latend_id, True)\
+        
+        end_time = time.time()
+        print('Time: %f s' % (end_time - start_time))
+        #save image
+        for i in range(img_id.shape[0]):
+            if i == 0:
+                row1 = img_id[i]
+                row2 = img_att[i]
+                row3 = img_fake[i]
+            else:
+                row1 = torch.cat([row1, img_id[i]], dim=2)
+                row2 = torch.cat([row2, img_att[i]], dim=2)
+                row3 = torch.cat([row3, img_fake[i]], dim=2)
+
+        #full = torch.cat([row1, row2, row3], dim=1).detach()
+        full = row3.detach()
+        full = full.permute(1, 2, 0)
+        output = full.to('cpu')
+        output = np.array(output)
+        output = output[..., ::-1]
+
+        output = output*255
+
+        cv2.imwrite(opt.output_path + 'result '+str(i)+'.jpg',output)
+
 
 
 def lcm(a, b): return abs(a * b) / fractions.gcd(a, b) if a and b else 0
@@ -26,11 +84,9 @@ detransformer = transforms.Compose([
         transforms.Normalize([0, 0, 0], [1/0.229, 1/0.224, 1/0.225]),
         transforms.Normalize([-0.485, -0.456, -0.406], [1, 1, 1])
     ])
-if __name__ == '__main__':
-    opt = TestOptions().parse()
 
-    start_epoch, epoch_iter = 1, 0
 
+def test_one_image(opt):
     torch.nn.Module.dump_patches = True
     model = create_model(opt)
     model.eval()
@@ -64,16 +120,7 @@ if __name__ == '__main__':
 
         ############## Forward Pass ######################
         img_fake = model(img_id, img_att, latend_id, latend_id, True)
-        print(img_fake.shape)
-        print("\n")
 
-        print(img_id.shape)
-        print("\n")
-        print(img_att.shape)
-        print("\n")
-        print(latend_id.shape)
-
-        print("\n")
         for i in range(img_id.shape[0]):
             if i == 0:
                 row1 = img_id[i]
@@ -94,3 +141,13 @@ if __name__ == '__main__':
         output = output*255
 
         cv2.imwrite(opt.output_path + 'result.jpg',output)
+
+def main(opt):
+    torch.nn.Module.dump_patches = True
+    model = create_model(opt)
+    benchmark(model, "./crop_224/*.jpg")
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()
+
+    main(opt)
