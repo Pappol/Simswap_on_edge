c041ec5902a1b4aed2523ad2f592219a78e5863a
Pappol <riccardo.parola@studenti.unitn.it>
2022-08-20 09:08:24 +0200

    update init

--- inizialize.sh
+++ inizialize.sh
@@ -1,11 +1,3 @@
-gh auth login
-
-git clone https://github.com/Pappol/simswap_on_edge
-
-cd simswap_on_edge 
-
-conda activate simswap 
-
 mkdir ./arcface_model
 
 pip install gdown
@@ -15,4 +7,8 @@ cd ./arcface_model
 gdown https://drive.google.com/u/0/uc?id=1TLNdIufzwesDbyr_nVTR7Zrx9oRHLM_N&export=download
 
 cd .. 
+gdown https://drive.google.com/u/0/uc?id=19pWvdEHS-CEG6tW3PdxdtZ5QEymVjImc&export=download
+
+tar -xvf vggface2_crop_arcfacealign_224.tar
+
 python python train.py --name v100 --batchSize 16
